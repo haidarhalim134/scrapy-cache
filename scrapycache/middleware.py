ed9f47da08115130f21f3a42d18b6b8c7dfd6e9f
haidar <haidarah134@gmail.com>
2025-01-13 09:31:15 +0700

    prefetch prestore check and no cache delay

--- scrapycache/middleware.py
+++ scrapycache/middleware.py
@@ -1,8 +1,10 @@
 from scrapy import Spider, Request
+from scrapy.exceptions import IgnoreRequest
 import os
 import datetime
 import json
 import sqlite3
+import time
 
 SPLITTER = '!!TIME!!'
 
@@ -17,6 +19,7 @@ class ScrapyCache:
         self.cache_enable = spider.custom_settings.get('cache_enable', False)
         self.cache_location = spider.custom_settings.get('cache_location', 'cache.db')
         self.cache_lifetime = spider.custom_settings.get('cache_lifetime', 60)
+        self.non_cache_delay = spider.custom_settings.get('non_cache_delay', 0)
         self.spider = spider
 
         if self.conn == None:
@@ -27,29 +30,41 @@ class ScrapyCache:
              (url TEXT, time TEXT, content TEXT)''')
 
         force_refresh = request.meta.get('force_refresh', False) or request.meta.get('_force_refresh', False)
+        prefetch_check = request.meta.get('cache_prefetch_check', lambda url, cache: True)
 
         if self.cache_enable and not force_refresh:
             
             url = request.url
             cache = self.find_cache(url)
 
-            if cache and ((datetime.datetime.now() - cache['time']).seconds < self.cache_lifetime or self.cache_lifetime == -1):
+            if cache and ((datetime.datetime.now() - cache['time']).seconds < self.cache_lifetime or self.cache_lifetime == -1) and prefetch_check(url, cache):
                 spider.logger.debug(f"cache used for <GET {url}>")
-                return request.replace(url=f'file:///{self.get_dummy_path()}', meta={**request.meta, '_force_refresh': True, '_original_url': url, '_data': cache}, dont_filter=True)
-            
+                return request.replace(url=f'file:////{self.get_dummy_path()}', meta={**request.meta, '_force_refresh': True, '_original_url': url, '_data': cache}, dont_filter=True)
+
+        if self.cache_enable and self.non_cache_delay > 0 and not request.url.startswith('file:///'):
+            spider.crawler.engine.pause()
+            time.sleep(self.non_cache_delay) 
+            spider.crawler.engine.unpause()
             
     def process_response(self, request, response, spider):
 
         if response.status == 200:
 
-            if not request.url.startswith('file:///'):
+            prestore_check = request.meta.get('cache_prestore_check', lambda url, cache: True)
 
-                self.update_data(request.url, response.text)
+            if not request.url.startswith('file:///'):
+                
+                if prestore_check(response.url, response.text):
+                    self.update_data(request.url, response.text)
 
             else:
                 response = response.replace(url=request.meta['_original_url'])
                 response = response.replace(body=request.meta['_data']['content'].encode('utf8'))
                 request.meta['_original_url'] = None
+
+        # TODO: remove later
+        with open("z-last.html", "w+") as fl:
+            fl.write(f"<div>{response.url}</div>" + response.text)
 
         request.meta['_force_refresh'] = False
 
